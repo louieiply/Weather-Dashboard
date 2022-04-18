fc0c169e77929d212ad748408addd7d1f2bd5948
louieiply <louieiply@gmail.com>
2022-04-18 23:48:29 +0930

    Implemented 5 day Forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,15 +9,13 @@ window.onload = function() {
     var wind = document.querySelector("#wind");
     var humidity = document.querySelector("#humidity");
     var uv = document.querySelector("#uv");
-
+    var cards_row = document.querySelector("#cards-row");
     var sample_city;
     var search;
-    var history_Moreinfo = new Array();
     var current_city = "";
-    var current_lon = 0;
-    var current_lat = 0;
-    var counter = 0;
-    baseURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
+    var current_lon = null;
+    var current_lat = null;
+    baseURL = "https://api.openweathermap.org/geo/1.0/direct?q=";
     query = "&limit=1&appid=2abfa3b21cf857688674d3c35b5dc15a";
 
 
@@ -37,11 +35,19 @@ window.onload = function() {
 
       async function getFirstData(url){
        await fetch(url)
-        .then((response) =>  {
-          return(response.json())})
+        .then((response) => { 
+          console.log(response);
+          return(response.json());})
         .then((data) =>  {
           console.log(data);
-          if(counter == 0){
+          if(data.length == ""){
+            debugger;
+            current_lon = null;
+            current_lat = null;
+            alert("Please check your input!!!"); 
+          }
+          else{
+            debugger;
             var button = document.createElement("button");
             city_row.append(button);
             button.setAttribute("type","button");
@@ -53,13 +59,10 @@ window.onload = function() {
             current_city =  data[0].name;
             current_lon = data[0].lon;
             current_lat = data[0].lat;
- 
-            // var cell = {city:data[0].name, lat:data[0].lat,lon:data[0].lon};
-            // history_Moreinfo.push(cell);
           }
         })
         .catch(err => {
-          // console.log(err);
+          console.log(err);
           
         });
         
@@ -71,21 +74,85 @@ window.onload = function() {
       await fetch(url)
       .then((response) =>  (response.json()))
       .then((data) =>  {
+        debugger;
+        console.log(data);
         cityNdate.textContent = current_city;
-        temp.textContent = "Temp: " + data.current.temp;
+        current_city = "";
+        temp.textContent = "Temp: " + data.current.temp +"°C";
         wind.textContent = "Wind: " + data.current.wind_speed;
-        humidity.textContent = "Humidity: " + data.current.humidity;
+        humidity.textContent = "Humidity: " + data.current.humidity + " %";
         uv.textContent = "UV Index: " + data.current.uvi;
         cardsCreate(data);
       })
       .catch(err => console.log(err));
     }
+  //   <div class="card col-2 mx-2 px-0" style="width: 10rem;">
+  //     <div class="card-body">
+  //       <h5 class="card-title">Card title</h5>
+  //       <i class='my-1 fas fa-cloud-rain' style="font-size:24px"></i>
+  //       <h6 class="card-subtitle my-3 text-muted">Temp</h6>
+  //       <h6 class="card-subtitle my-3 text-muted">Wind</h6>
+  //       <h6 class="card-subtitle my-3 text-muted">Humidity</h6>
+  //     </div>
+  //   </div>
+
 
 
     function cardsCreate(data) {
-      for (let index = 0; index < 5; index++) {
-        const element = array[index];
-        
+      if(document.querySelectorAll(".card").length == ""){
+        for (let index = 0; index < 5; index++) {
+          debugger;
+          var card = document.createElement("div");
+          var card_body = document.createElement("div");
+          var card_date = document.createElement("h5");
+          var card_temp = document.createElement("h6");
+          var card_wind = document.createElement("h6");
+          var card_humidity = document.createElement("h6");
+          card.classList.add("card");
+          card.classList.add("col-2");
+          card.classList.add("mx-1");
+          card.classList.add("px-0");
+          card.style.width = "10rem";
+          cards_row.append(card);
+          card_body.classList.add("card-body");
+          card.append(card_body);
+          card_date.id = "card"+"date"+index;
+          let fullDate = new Date(data.daily[index].dt*1000);          
+          card_date.textContent = fullDate.getDay()+"/"+fullDate.getMonth()+"/"+fullDate.getFullYear();
+          card_date.classList.add("card-title");
+          card_body.append(card_date);
+          card_temp.id = "card"+"temp"+index;
+          card_temp.textContent = "Temp: " + data.daily[index].temp.day +" °C";
+          card_temp.classList.add("card-subtitle");
+          card_temp.classList.add("my-3");
+          card_temp.classList.add("text-muted");
+          card_body.append(card_temp);
+          card_wind.id = "card"+"wind"+index;
+          card_wind.textContent = "Wind: " + data.daily[index].wind_speed + " MPH";
+          card_wind.classList.add("card-subtitle");
+          card_wind.classList.add("my-3");
+          card_wind.classList.add("text-muted");
+          card_body.append(card_wind);
+          card_humidity.id = "card"+"humidity"+index;
+          card_humidity.textContent ="UV Index: " + data.daily[index].humidity + " %";
+          card_humidity.classList.add("card-subtitle");
+          card_humidity.classList.add("my-3");
+          card_humidity.classList.add("text-muted");
+          card_body.append(card_humidity);
+        }
+      }
+      else{
+        for (let index = 0; index < 5; index++) {
+          var carddate = document.querySelector("#carddate"+index);
+          var cardtemp = document.querySelector("#cardtemp"+index);
+          var cardwind = document.querySelector("#cardwind"+index);
+          var cardhumidity = document.querySelector("#cardhumidity"+index);
+          let fullDate = new Date(data.daily[index].dt*1000);      
+          carddate.textContent = fullDate.getDay()+"/"+fullDate.getMonth()+"/"+fullDate.getFullYear();
+          cardtemp.textContent = "Temp: " + data.daily[index].temp.day +" °C";
+          cardwind.textContent = "Wind: " + data.daily[index].wind_speed + " MPH";
+          cardhumidity.textContent = "UV Index: " + data.daily[index].humidity + " %";
+        }
       }
     }
 
@@ -96,7 +163,9 @@ window.onload = function() {
      CheckLocalStorage();
 
 
-     search_city.addEventListener("click",function(e){
+     search_city.addEventListener("click", async function(e){
+      debugger;
+      e.preventDefault();
       e.stopPropagation();
       console.log(JSON.stringify(search_input.value)); 
       if(search_input.value.replace(" ","") == ""){
@@ -105,10 +174,16 @@ window.onload = function() {
       else{
         search = search_input.value; 
         search_input.value = null;
-        var finalUrl =baseURL+search+query;
-        getFirstData(finalUrl);
-        getDetailedData(current_lon,current_lat);
-        
+        var finalUrl = baseURL+search+query;
+        await getFirstData(finalUrl);
+        debugger;
+        if(current_lon !== null && current_lat !== null){
+          debugger;
+        await  getDetailedData(current_lon,current_lat);
+        }
+        else{
+          alert("error");
+        }
       }
 
      });
