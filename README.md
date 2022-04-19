86393d1d4b59d6cf3550969210cd3ef7ac6fb4bc
louieiply <louieiply@gmail.com>
2022-04-19 13:51:43 +0930

    Modify Readme file and removed awesome font css link

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Weather-Dashboard
+## Description
+
+This is a weather dashboard
+
+
+
+## Table of Contents
+
+-[Installation]
+-[Usage]
+-[Credit]
+-[License]
+-[Feature]
+
+## Installation
+
+There are no steps required for installation. You only require a web browser of the following (Chrome, Brave, Edge, etc.) to open up the webpage to view the content.
+
+## Usage
+![screenshot 1](../screenshots/Screenshot1.png)
+
+![screenshot 2](../screenshots/Screenshot2.png)
+
+## Credit
+
+Louie Ip, GitHub for storage and hosting, Bootstrap for providing some of the css look, OpenWeather API for all the weather information
+
+## License
+ 
+Open Weather Free API key
+
+## Features
+
+Weather dashboard allows you to get more information about weather forecast of the city you desire to know.
+
+## Deployed Link
+
+
+
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-
+//Varaibles Declaration
     var city_row = document.querySelector("#city-row");
     var search_city = document.querySelector("#search");
     var search_input = document.querySelector("#city-search");
@@ -21,7 +21,7 @@ window.onload = function() {
     baseURL = "https://api.openweathermap.org/geo/1.0/direct?q=";
     query = "&limit=1&appid=2abfa3b21cf857688674d3c35b5dc15a";
 
-
+    // Check LocalStorage whether empty or not(with Async make sure fetch API return data before starting another one)
     var CheckLocalStorage = async function(){
       if(localStorage.getItem("history") == null || localStorage.getItem("history") == ""){
           return;
@@ -37,7 +37,8 @@ window.onload = function() {
         
       }
     }
-
+      //execute on Search click and page onload 
+      //First API gets longitude and latitude by city search
       async function getFirstData(url){
         var result = null;
         await fetch(url)
@@ -46,12 +47,14 @@ window.onload = function() {
           return(response.json());})
         .then((data) =>  {
           console.log(data);
+          //if API return [] empty
           if(data.length == ""){
             debugger;
             current_lon = null;
             current_lat = null;
             alert("Please check your input!!!"); 
           }
+          //return and create history button
           else{
             debugger;
             var button = document.createElement("button");
@@ -68,6 +71,7 @@ window.onload = function() {
             current_city =  data[0].name;
             current_lon = data[0].lon;
             current_lat = data[0].lat;
+            //Add Eventlistener for each History button tigger by click
             button.addEventListener("click",function(e){
               e.stopPropagation();
               e.preventDefault();
@@ -75,25 +79,28 @@ window.onload = function() {
               current_city = e.target.value;
             });
             var templength = JSON.parse(localStorage.getItem("history"));
-            // alert(templength.length);
-
             result = data[0].name;
             counter++;
+            //check if localstorage history items being all loaded 
             if(counter > localStorageLength){
-              if(templength.length >= 10){
-                var list = JSON.parse(localStorage.getItem("history"));
-                list.splice(0,1);
-                console.log(list);
-                localStorage.setItem("history",JSON.stringify(list));
-                var firstButton = document.querySelector(".history-btn");
-                firstButton.remove();
-              }
-
+              //localStorage
               if(localStorage.getItem("history") == null || localStorage.getItem("history") == ""){
                 var list = [data[0].name];
                 localStorage.setItem("history",JSON.stringify(list));
               }
+              //if localstorage history is not null
               else{
+                //Check if realtime localStorage history length reach to 10
+                if(templength.length >= 10){
+                  var list = JSON.parse(localStorage.getItem("history"));
+                  //remove first element of list
+                  list.splice(0,1);
+                  console.log(list);
+                  //save list (PS: if I have more time I would merge this condition with the buttom save list codes)
+                  localStorage.setItem("history",JSON.stringify(list));
+                  var firstButton = document.querySelector(".history-btn");
+                  firstButton.remove();
+                }
                 var list = JSON.parse(localStorage.getItem("history"));
                 list.push(data[0].name);
                 console.log(list);
@@ -109,7 +116,8 @@ window.onload = function() {
         return result;
       }
 
-
+    //Load data into Weather Display Panel
+    //Second API gets weather info by longitude and latitude
     async function getDetailedData(lon,lat){
       url = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&units=metric&exclude=hourly,minutely&appid=2abfa3b21cf857688674d3c35b5dc15a"
       await fetch(url)
@@ -129,8 +137,9 @@ window.onload = function() {
     }
 
 
-
+    //Load and Create 5-day Forecast weather card
     function cardsCreate(data) {
+      //run if card doesn't exist
       if(document.querySelectorAll(".card").length == ""){
         for (let index = 0; index < 5; index++) {
           debugger;
@@ -177,6 +186,7 @@ window.onload = function() {
           card_body.append(card_humidity);
         }
       }
+      //if cards are existing
       else{
         for (let index = 0; index < 5; index++) {
           var carddate = document.querySelector("#carddate"+index);
@@ -202,22 +212,23 @@ window.onload = function() {
 
 
 
-
+    //Search Button eventlistner when click triggers
      search_city.addEventListener("click", async function(e){
       debugger;
       e.preventDefault();
       e.stopPropagation();
-      // console.log(JSON.stringify(search_input.value));
+      //check if search input only contain spaces
       if(search_input.value.replace(" ","") == ""){
         return;
       }
+      // if search not empty
       else{
         search = search_input.value; 
         search_input.value = null;
         var finalUrl = baseURL+search+query;
         var result = await getFirstData(finalUrl);
         debugger;
-
+        // if longitude and latitude is not null then run
         if(current_lon !== null && current_lat !== null){
           debugger;
         await getDetailedData(current_lon,current_lat);
